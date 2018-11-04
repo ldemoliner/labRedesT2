40e6158eb581bee484f37e5a81fd78c386e54644
Lucas Demoliner <Lucas Demoliner>
2018-11-04 21:55:59 -0200

    Ajustando rota de destino e rota local

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
+//HOW TO START
+//node app.js <config.txt> <mensagem> <seu endereço>
+//node app.js <withToken.txt> <messages2.txt> <127.0.0.1:6000>
+
 //TO DO
 /*
+-TOKEN NÃO ESTA PASSANDO, SÓ DEPOIS DE ENVIAR TODAS MENSAGENS
 -DEIXAR O USUARIO ESCREVER SUA MENSAGEM E PARA QUEM DESEJA ENVIAR A MENSAGEM
 -MUDAR CONTROLE DE ERRO
--PORT E HOST DEVE SER LIDO DO ARQUIVO CONFIG E NÃO ESCRITO PELO TERMINAL
 -ENVIAR MENSAGEM BROADCAST(TODOS)
 */
 
@@ -18,13 +22,13 @@ var content = fs.readFileSync(process.argv[2], 'utf8');
 content = content.split("\r\n")
 content[0] = content[0].split(":")
 content[0][1] = parseInt(content[0][1])
-var PORT = content[0][1];
-var HOST = content[0][0];
+var DESTINATION_PORT = content[0][1];
+var DESTINATION_HOST = content[0][0];
 var NAME = content[1];
 var TIME = content[2]*1000;
-var TOKE = content[3];
-var NEXT = process.argv[4].split(":");
-NEXT[1] = parseInt(NEXT[1]);
+var TOKEN = content[3];
+var ADRESS = process.argv[4].split(":");
+ADRESS[1] = parseInt(ADRESS[1]);
 
 //READ MESSAGES IN QUEUE
 var queue = fs.readFileSync(process.argv[3], 'utf8');
@@ -43,44 +47,44 @@ server.on('listening', function () {
 //SERVER SENDING MESSAGES
 server.on('message', function (message, remote) {
     console.log(remote.address + ':' + remote.port +' - ' + message);
-    // setTimeout(() => {server.send(message, 0, message.length, NEXT[1], NEXT[0], (err, bytes) => {});}, TIME)
+    // setTimeout(() => {server.send(message, 0, message.length, DESTINATION_PORT, DESTINATION_HOST, (err, bytes) => {});}, TIME)
     //IF HAVE TOKEN
     if(isToken(message)){
         //IF SHOULD WRITE
         if(shouldWrite() && queue.length > 0){
             var tuple = queue.pop()
             var package = `2345;naocopiado:${NAME}:${tuple[1]}:M:${tuple[0]}`
-            setTimeout(() => {server.send(package, 0, package.length, NEXT[1], NEXT[0], (err, bytes) => {})}, TIME)
+            setTimeout(() => {server.send(package, 0, package.length, DESTINATION_PORT, DESTINATION_HOST, (err, bytes) => {})}, TIME)
         } else {
-            setTimeout(() => {server.send(message, 0, message.length, NEXT[1], NEXT[0], (err, bytes) => {});}, TIME)
+            setTimeout(() => {server.send(message, 0, message.length, DESTINATION_PORT, DESTINATION_HOST, (err, bytes) => {});}, TIME)
         }
         //IF I SEND MESSAGE
     } else if(itsMe(message)){
         console.log("Mensagem retornou com sucesso!")
         var token = Buffer("1234")
-        setTimeout(() => {server.send(token, 0, token.length, NEXT[1], NEXT[0], (err, bytes) => {})}, TIME)
+        setTimeout(() => {server.send(token, 0, token.length, DESTINATION_PORT, DESTINATION_HOST, (err, bytes) => {})}, TIME)
         //IF MESSAGE IS FOR ME
     } else if(forMe(message)) {
         console.log("Mensagem recebida!")
         message = message + ""
         console.log(message)
-        var origin = message.split(":")[2];
+        var origin = message.split(":")[1];
         var mail = message.split(":")[4];
         console.log(origin + " " + mail)
         var token = Buffer("1234")
-        setTimeout(() => {server.send(token, 0, token.length, NEXT[1], NEXT[0], (err, bytes) => {})}, TIME)
+        setTimeout(() => {server.send(token, 0, token.length, DESTINATION_PORT, DESTINATION_HOST, (err, bytes) => {})}, TIME)
     } else {
         console.log("Repassando a mensagem");
-        setTimeout(() => {server.send(message, 0, message.length, NEXT[1], NEXT[0], (err, bytes) => {})}, TIME)
+        setTimeout(() => {server.send(message, 0, message.length, DESTINATION_PORT, DESTINATION_HOST, (err, bytes) => {})}, TIME)
     }
 });
 
-server.bind(PORT, HOST);
+server.bind(ADRESS[1], ADRESS[0]);
 
 //VERIFY TOKEN
-if(TOKE === "true"){
+if(TOKEN === "true"){
     message = Buffer("1234")
-    server.send(message, 0, message.length, NEXT[1], NEXT[0], (err, bytes) => {});
+    server.send(message, 0, message.length, DESTINATION_PORT, DESTINATION_HOST, (err, bytes) => {});
 }
 
 function isToken(message){
